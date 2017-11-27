3c18cb4b2eaa84fdf5e0b5cd502ac5a25f6cd4c3
Dmitry Rubinstein <rubinsteindb@gmail.com>
2017-11-27 05:12:39 +0300

    Release 0.1.0

--- mix.exs
+++ mix.exs
@@ -5,8 +5,10 @@ defmodule Agala.Provider.Telegram.Mixfile do
     [
       app: :agala_telegram,
       version: "0.1.0",
-      elixir: "~> 1.4",
+      elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -21,12 +23,27 @@ defmodule Agala.Provider.Telegram.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:agala, github: "agalaframework/agala", branch: "release/2.0.0"},
+      {:agala, "~> 2.0.1"},
       {:httpoison, "~> 0.12"},
       {:poison, "~> 3.1"},
       {:ex_doc, "~> 0.16", only: :dev},
       {:inch_ex,"~> 0.5", only: :docs},
       {:credo, "~> 0.8", only: [:dev, :test]}
+    ]
+  end
+
+  defp description do
+    """
+    Telegram provider for Agala framework.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Dmitry Rubinstein"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/agalaframework/agala_telegram"},
+      files: ~w(mix.exs README* CHANGELOG* lib)
     ]
   end
 end
